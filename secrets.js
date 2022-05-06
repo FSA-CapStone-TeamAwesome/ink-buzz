8a614144ba07d18c02850d6a972f79684619f1a5
Don Romaniello <53401920+DonRomaniello@users.noreply.github.com>
2022-05-06 10:38:06 -0400

    Delete secrets.js
    Secret used to mean something in this country.

--- secrets.js
+++
@@ -1,8 +0,0 @@
-export const apiKey = "AIzaSyAdHg5r7EkFvjVV9ZKkfW2htooKPUE_FxA";
-export const authDomain = "ink-buzz.firebaseapp.com";
-export const databaseURL = "https://ink-buzz-default-rtdb.firebaseio.com";
-export const projectId = "ink-buzz";
-export const storageBucket = "ink-buzz.appspot.com";
-export const messagingSenderId = "473232897117";
-export const appId = "1:473232897117:web:ae18f9bc7edba8f9b1c9b4";
-export const measurementId = "G-HJWGDQPCCS";
