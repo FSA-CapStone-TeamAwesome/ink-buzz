c330257bb9256ebfdca96c91ab04077b1cf67637
Don Romaniello <don.romaniello@gmail.com>
2022-05-07 12:00:44 -0400

    Broken.

--- screens/StorageApp.js
+++ screens/StorageApp.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { getStorage, ref, getDownloadURL, getBlob } from "firebase/storage";
 import app from "../config/firebase"
 
@@ -11,33 +12,32 @@ export default function StorageApp() {
   const storage = getStorage(app);
   const image = ref(storage, 'images/universal/demoTattoo.png');
 
-  let newUrl = ''
+  // const [ newUrl ] = useState("");
+
+  let newUrl = '';
 
   // getDownloadURL(image).then((url) => {
   //   console.log("This is the url")
   //   console.log(url)
   // })
 
-
-
-
-
   getBlob(image).then((blob) => {
-      let newUrl = URL.createObjectURL(blob)
-      console.log(typeof(newUrl))
-
-
-
+    newUrl = URL.createObjectURL(blob)
+    console.log(newUrl)
   })
 
-  //console.log("Now")
+
+  useEffect(() => {
+    console.log(newUrl)
+  }, [])
 
 
 
 
   return (
     <View style={styles.container}>
-      <Text>Storage module is working.</Text>
+      <Image source={{ uri: newUrl}} />
+      <Text>Storage module is working. {newUrl} Is it?</Text>
     </View>
   );
 }
