ea0350b44141b4df7af3bab319f951d3d3dff268
Don Romaniello <don.romaniello@gmail.com>
2022-05-08 10:18:11 -0400

    Upload tests. Upload is reaching server, but content is not as expected.

--- screens/CameraApp.js
+++ screens/CameraApp.js
@@ -1,10 +1,12 @@
 import { useState, useEffect, setState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { getStorage, ref, getBlob, uploadBytes } from "firebase/storage";
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+
 import app from "../config/firebase"
 
 
-export default function StorageApp() {
+export default function CameraApp() {
 
   // Firebase 9 is very modular, so you invoke different
   // modules as needed. The configured app is passed as an
@@ -13,7 +15,7 @@ export default function StorageApp() {
   const storage = getStorage(app);
 
   //const image = ref(storage, 'images/universal/demoTattooWarped.png');
-  const image = ref(storage, 'images/universal/demoTattoo.png');
+  // const image = ref(storage, 'images/universal/demoTattoo.png');
 
   const uploadImageRef = ref(storage, 'images/universal/uploadTests/test1.png');
 
@@ -22,12 +24,6 @@ export default function StorageApp() {
 
   const [uploadImageSource, setUploadImageSource] = useState('assets/UploadTest.png');
 
-  useEffect(() => {
-    getBlob(image).then((blob) => {
-      const useUrl = URL.createObjectURL(blob);
-      setImageUrl(useUrl)
-    })
-  }, [])
 
   useEffect(() => {
     uploadBytes(uploadImageRef, uploadImageSource).then((snapshot) => {
@@ -37,9 +33,7 @@ export default function StorageApp() {
 
   return (
     <View style={styles.container}>
-      <Image source={{ uri: imageUrl}}
-      style={{width: 400, height: 400}} />
-      <Text>Storage module: {imageUrl} </Text>
+      <Text>Camera Module </Text>
     </View>
   );
 }
