89b41bea2104f3e3c17031639d53ac54f02cae84
Don Romaniello <don.romaniello@gmail.com>
2022-05-11 12:10:40 -0400

    Noncing.

--- navigation/screens/WalletScreen.js
+++ navigation/screens/WalletScreen.js
@@ -5,25 +5,30 @@ import WalletConnectProvider, {
   useWalletConnect,
 } from "react-native-walletconnect";
 
+const noncer = () => {
+  return toString(Date.now())
+}
+
 const WalletConnectExample = () => {
-  const { createSession, killSession, session, signTransaction } =
+  const { createSession, killSession, session, sendTransaction } =
     useWalletConnect();
   const hasWallet = !!session.length;
+  console.log(session)
   return (
     <>
       {!hasWallet && <Button title="Connect" onPress={createSession} />}
       {!!hasWallet && (
         <Button
-          title="Sign Transaction"
+          title="Send Transaction"
           onPress={() =>
-            signTransaction({
-              from: "0xbc28Ea04101F03aA7a94C1379bc3AB32E65e62d3",
-              to: "0x89D24A7b4cCB1b6fAA2625Fe562bDd9A23260359",
-              data: "0x",
-              gasPrice: "0x02540be400",
-              gas: "0x9c40",
-              value: "0x00",
-              nonce: "0x0114",
+            sendTransaction({
+              from: "0x35aa69a3b9a11e814cb405f4b15bd83375c25da7",
+              to: "0x73de20c61d696867a656b089762ad52342dc365e",
+              data: "0x636F6E74616374",
+              // gasPrice: "200000000000",
+              // gas: "1",
+              value: "100000000000000000", // in Wei, or 10^-18 ETH
+              nonce: noncer(),
             })
           }
         />
