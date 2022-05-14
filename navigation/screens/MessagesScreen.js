86c2c2847e5e3b5ea639beb1f908bc4467e1f946
Don Romaniello <don.romaniello@gmail.com>
2022-05-13 22:39:28 -0400

    Messages writing.

--- navigation/screens/MessagesScreen.js
+++ navigation/screens/MessagesScreen.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { auth, db, app } from "../../config/firebase";
+
+import { doc, setDoc } from "firebase/firestore";
+
+
+
+
+
 const MessagesScreen = ({ navigation }) => {
+
+
+
+
+  console.log(auth.currentUser.uid)
+
+  const recipient = 'L814iNPsM1h7WE99xnRi0v74zFI3'
+
+
+  setDoc(doc(db,
+    `messages/queue/${recipient}`, 'message'),
+    {artReference: null,
+    content: "Yo and lo.",
+    from: auth.currentUser.uid,
+    photoUrl: null,
+    timestamp: Date.now()})
+
+
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text
