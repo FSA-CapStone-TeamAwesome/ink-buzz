04ff7d5d1a8872ea8576e5f9c8fa1d83df5dea0e
Don Romaniello <don.romaniello@gmail.com>
2022-05-14 08:40:18 -0400

    Button sends hard-coded message.

--- navigation/screens/MessagesScreen.js
+++ navigation/screens/MessagesScreen.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 import { auth, db, app } from "../../config/firebase";
 
 import { collection, addDoc, Timestamp } from "firebase/firestore";
+
+
+// userIds
+
+// chat2@chat.com
+// L814iNPsM1h7WE99xnRi0v74zFI3
+
+// chat1@chat.com
+// JotxkdT73WZxdfVuw00itwp2GWr1
 
 
 
@@ -16,17 +25,18 @@ const MessagesScreen = ({ navigation }) => {
   const recipient = 'L814iNPsM1h7WE99xnRi0v74zFI3'
 
 
+  const sendMessage = () => {
 
+    addDoc(collection(db,
+      `messages/queue/${recipient}`),
+      {artReference: null,
+      content: "Yo and lo.",
+      fromName: auth.currentUser.email,
+      fromId: auth.currentUser.uid,
+      photoUrl: null,
+      timestamp: Timestamp.fromMillis(Date.now())})
 
-  addDoc(collection(db,
-    `messages/queue/${recipient}`),
-    {artReference: null,
-    content: "Yo and lo.",
-    fromName: auth.currentUser.email,
-    fromId: auth.currentUser.uid,
-    photoUrl: null,
-    timestamp: Timestamp.fromMillis(Date.now())})
-
+  }
 
 
   return (
@@ -36,6 +46,7 @@ const MessagesScreen = ({ navigation }) => {
         style={{ fontSize: 26, fontWeight: 'bold' }}>
         Messages Screen
       </Text>
+      <Button title='Send' onPress={() => {sendMessage()}} />
     </View>
   );
 };
